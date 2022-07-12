edaad9a312f1b5e7ac85e3b74be05530cd14f71c
Daiyan Rafi <daiyanrafi1@gmail.com>
2022-07-12 18:16:16 +0600

    okay

---
+++ ridly/Data/Application DB Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ridly.Data
+{
+    public class Application_DB_Context :DbContext
+    {
+        public Application_DB_Context(DbContextOptions<Application_DB_Context> options) : base(options) 
+        {
+        }
+
+        public DbSet<Category>? Categories { get; set; }
+    }
+
+}
---
+++ ridly/Data/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace ridly.Data
+{
+    public class Category
+    {
+    }
+}
